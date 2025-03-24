32ef65104c513492b5c2e0dadf27e9da745a9df6
TuiTenTruong <truong0795432149@gmail.com>
2025-03-24 20:18:58 +0700

    update

--- knn.py
+++ knn.py
@@ -1,8 +1,11 @@
+from matplotlib import pyplot as plt
 from pandas import read_csv
 from sklearn import neighbors
 from sklearn.calibration import LabelEncoder
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import seaborn as sns
 columns = [
     'class', 'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 
     'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 
@@ -11,6 +14,7 @@ columns = [
     'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat'
 ]
 df = read_csv('agaricus-lepiota.data', names=columns);
+
 df.replace("?", df.mode().iloc[0], inplace=True)
 
 # Tiền xử lý: Chuyển đổi các giá trị phân loại thành giá trị số
@@ -21,6 +25,11 @@ for col in df.columns:
 
 X = df.drop('class', axis=1)
 y = df['class'];
+for i in range(2):
+    print(f"sample data of class {i}:")
+    print(X[y==i].head())
+    print()
+
 Accuracies = [];
 for i in range(0,10):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=40+i)
@@ -28,6 +37,10 @@ for i in range(0,10):
     model_KNN.fit(X_train, y_train)
 
     y_pred = model_KNN.predict(X_test)
+    if(i==2):
+        print("Print results for 20 test data points")
+        print("Predicted labels :", ', '.join(map(str, y_pred[0:20])))
+        print("Ground truth     :", ', '.join(map(str, y_test[0:20])))
 
     from sklearn.metrics import accuracy_score
     accuracy = accuracy_score(y_test, y_pred);
@@ -36,7 +49,18 @@ for i in range(0,10):
 
 mean_accuracy = sum(Accuracies) / len(Accuracies);
 print("Average accuracy of KNN: %.2f %%" %(100 * mean_accuracy))
+cm = confusion_matrix(y_test, y_pred, labels=[0, 1])
 
+# Convert confusion matrix to a DataFrame for better visualization
+cm_df = pd.DataFrame(cm, index=['Poisonous', 'Edible'], columns=['Poisonous', 'Edible'])
+
+# Display confusion matrix
+plt.figure(figsize=(5, 4))
+sns.heatmap(cm_df, annot=True, fmt="d", cmap="Blues")
+plt.title('Confusion Matrix')
+plt.ylabel('Actual Values')
+plt.xlabel('Predicted Values')
+plt.show()
 
  
 
